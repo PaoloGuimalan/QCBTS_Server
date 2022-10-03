7c44c1fc1e3832664d40fc15511d9942d55f4c8c
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-10-03 06:22:15 -0900

    Company Data and Company Admin Data has now been Separated with Company Registration and Company Admin adding features that allow multiple admin in one company.

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -9,6 +9,7 @@ const jwt = require("jsonwebtoken")
 
 const AdminData = require("../../schema/admin/admindata")
 const CompanyData = require("../../schema/company/companydata")
+const CompanyRegdata = require("../../schema/company/companyRegdata")
 
 router.use((req, res, next) => {
     next();
@@ -105,27 +106,31 @@ router.get('/testadmin', (req, res) => {
 })
 
 router.post('/createcompanyadmin', jwtverifier, async (req, res) => {
-    const companyID = `company_${makeid(6)}`;
+    const companyID = req.body.companyID;
+    const companyAdminID = `companyadmin_${makeid(6)}`;
     const companyName = req.body.companyName;
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
-    const companyNumber = req.body.companyNumber;
-    const preview = req.body.preview;
+    // const companyNumber = req.body.companyNumber;
+    // const companyAddress = req.body.companyAddress;
+    // const preview = req.body.preview;
     const dateRegistered = dateGetter();
     const email = req.body.email;
     const password = req.body.password;
 
-    const saveData = (compID) => {
+    const saveData = (compID, compAdID) => {
 
         const newCompany = new CompanyData({
             companyID: compID,
+            companyAdminID: compAdID,
             companyName: companyName,
             companyAdmin: {
                 firstname: firstname,
                 lastname: lastname
             },
-            companyNumber: companyNumber,
-            preview: preview,
+            // companyNumber: companyNumber,
+            // companyAddress: companyAddress,
+            // preview: preview,
             status: false,
             dateRegistered: dateRegistered,
             email: email,
@@ -134,27 +139,54 @@ router.post('/createcompanyadmin', jwtverifier, async (req, res) => {
 
         newCompany.save().then(() => {
             res.send({status: true, result: {
-                message: "New Company Added!"
+                message: "New Company Admin Added!"
             }})
+        }).catch((err) => {
+            res.send({status: false, result: {
+                message: "Unable to save new admin!"
+            }})
+            console.log(err);
         })
     }
 
-
-    const dataCheck = (id) => {
-        CompanyData.findOne({companyID: id}, (err, result) => {
+    const dataAdminCheck = (adID, cadID) => {
+        CompanyData.findOne({companyAdminID: adID}, (err, result) => {
             if(err){
                 res.send({status: false, result: { message: "Error creating Company Admin!" }})
                 console.log(err)
             }
             else{
                 if(result == null){
-                    saveData(id)
+                    saveData(cadID, adID)
                 }
                 else{
-                    dataCheck(`company_${makeid(6)}`)
+                    dataAdminCheck(`companyadmin_${makeid(6)}`, cadID)
                 }
             }
         })
+    }
+
+
+    const dataCheck = (id) => {
+        if(id == null){
+            dataAdminCheck(companyAdminID ,`company_${makeid(6)}`)
+        }
+        else{
+            CompanyRegdata.findOne({companyID: id}, (err, result) => {
+                if(err){
+                    res.send({status: false, result: { message: "Error creating Company Admin!" }})
+                    console.log(err)
+                }
+                else{
+                    if(result == null){
+                        res.send({status: false, result: { message: "No Company Matched with the Company ID provided!" }})
+                    }
+                    else{
+                        dataAdminCheck(`companyadmin_${makeid(6)}`, id)
+                    }
+                }
+            })
+        }
     }
 
     dataCheck(companyID)
@@ -179,16 +211,80 @@ router.get('/companylist', jwtverifier, (req, res) => {
 
 router.post('/updatecompanystatus', jwtverifier, (req, res) => {
     const id = req.params.decodedID;
-    const companyID = req.body.companyID;
+    const companyAdminID = req.body.companyAdminID;
     const status = req.body.status;
 
-    CompanyData.updateOne({ companyID: companyID }, { status: status }, (err, result) => {
+    CompanyData.updateOne({ companyAdminID: companyAdminID }, { status: status }, (err, result) => {
         if(err){
             console.log(err);
             res.send({ status: false, result: { message: `${status? "Activation" : "Deactivation"} was interrupted!` } })
         }
         else{
-            res.send({ status: true, result: { message: `${companyID} has been ${status? "Activated" : "Deactivated"}` } })
+            res.send({ status: true, result: { message: `${companyAdminID} has been ${status? "Activated" : "Deactivated"}` } })
+        }
+    })
+})
+
+router.post('/createcompanyreg', jwtverifier, (req, res) => {
+    const id = req.params.decodedID;
+    const companyID = `company_${makeid(6)}`;
+    const companyName = req.body.companyName;
+    const companyAddress = req.body.companyAddress;
+    const companyNumber = req.body.companyNumber;
+    const email = req.body.companyEmail;
+    const dateRegistered = dateGetter();
+    const preview = req.body.preview;
+
+    const saveData = (finalID) => {
+        const newData = new CompanyRegdata({
+            companyID: finalID,
+            companyName: companyName,
+            companyAddress: companyAddress,
+            companyNumber: companyNumber,
+            email: email,
+            dateRegistered: dateRegistered,
+            preview: preview
+        });
+
+        newData.save().then(() => {
+            res.send({status: true, result: {
+                message: "New Company Added!"
+            }})
+        })
+    }
+
+    const dataCheck = (cmpID) => {
+        CompanyRegdata.findOne({companyID: cmpID}, (err, result) => {
+            if(err){
+                res.send({status: false, result:{
+                    message: "Error Validating Data!"
+                }})
+            }
+            else{
+                if(result == null){
+                    //good
+                    saveData(cmpID)
+                }
+                else{
+                    dataCheck(`company_${makeid(6)}`)
+                }
+            }
+        })
+    }
+
+    dataCheck(companyID)
+})
+
+router.get('/companyreglist', jwtverifier, (req, res) => {
+    const id = req.params.decodedID;
+
+    CompanyRegdata.find({}, (err, result) => {
+        if(err){
+            res.send({status: false, result: { message: "Cannot fetch list!" }})
+            console.log(err);
+        }
+        else{
+            res.send({status: true, result: result})
         }
     })
 })
--- schema/company/companydata.js
+++ schema/company/companydata.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const companydata = mongoose.Schema({
     companyID: {type: mongoose.Schema.Types.Mixed, required: true},
+    companyAdminID: {type: mongoose.Schema.Types.Mixed, required: true},
     companyName: {type: mongoose.Schema.Types.Mixed, required: true},
     companyAdmin: {
         firstname: {type: mongoose.Schema.Types.Mixed, required: true},
         lastname: {type: mongoose.Schema.Types.Mixed, required: true}
     },
-    companyNumber: {type: mongoose.Schema.Types.Mixed, required: true},
-    preview: {type: mongoose.Schema.Types.Mixed, required: true},
     status: {type: Boolean, required: true},
     dateRegistered: {type: mongoose.Schema.Types.Mixed, required: true},
     email: {type: mongoose.Schema.Types.Mixed, required: true},
