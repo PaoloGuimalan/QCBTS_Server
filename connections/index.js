4548712ca41eb20c5bb5013df254fa13fd54c3a3
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-11-26 21:52:47 -0900

    Mongo DB link url removed retryWrites for cyclic compatibility

--- connections/index.js
+++ connections/index.js
@@ -1,5 +1,17 @@
+//Heroku
+
+// module.exports = {
+//     url: 'mongodb+srv://dt187:demonterror187@cluster0.6uzwm.mongodb.net/qcbtsdatabase?retryWrites=true&w=majority',
+//     params:{
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// }
+
+//Cyclic
+
 module.exports = {
-    url: 'mongodb+srv://dt187:demonterror187@cluster0.6uzwm.mongodb.net/qcbtsdatabase?retryWrites=true&w=majority',
+    url: 'mongodb+srv://dt187:demonterror187@cluster0.6uzwm.mongodb.net/qcbtsdatabase?w=majority',
     params:{
         useNewUrlParser: true,
         useUnifiedTopology: true
