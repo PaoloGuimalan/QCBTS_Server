3a40607b38d11ccaff7570c52cc0d8e04eabbe78
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-12-07 02:21:07 -0900

    Create Route and Route List route for Company Admin has been Added

--- routes/company/index.js
+++ routes/company/index.js
@@ -6,10 +6,31 @@ const jwt = require("jsonwebtoken")
 const CompanyData = require("../../schema/company/companydata");
 const CompanyRegdata = require("../../schema/company/companyRegdata")
 const BusStopsData = require("../../schema/configs/busstops")
+const RoutesData = require("../../schema/configs/routes")
 
 router.use((req, res, next) => {
     next();
 })
+
+function makeid(length) {
+    var result           = '';
+    var characters       = '0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+function dateGetter(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    return today = mm + '/' + dd + '/' + yyyy;
+}
 
 const jwtverifier = (req, res, next) => {
     const token = req.headers["x-access-token"];
@@ -101,6 +122,46 @@ router.get('/enabledBusStops', jwtverifier, (req, res) => {
         if(err){
             res.send({ status: false, result: { message: "Cannot fetch Bus Stops" } })
             console.log(err)
+        }
+        else{
+            res.send({ status: true, result: result })
+        }
+    })
+})
+
+router.post('/createRoute', jwtverifier, (req, res) => {
+    const id = req.params.decodedID;
+    const routeName = req.body.routeName;
+    const stationList = req.body.stationList;
+    const routePath = req.body.routePath;
+
+    const newRoute = new RoutesData({
+        routeID: `RT_${makeid(8)}`,
+        routeName: routeName,
+        stationList: stationList,
+        routePath: routePath,
+        dateAdded: dateGetter(),
+        addedBy: id,
+        status: false
+    })
+
+    newRoute.save().then(() => {
+        res.send({ status: true, result: { message: "New Route has been Saved!" } })
+    }).catch((err) => {
+        res.send({ status: false, result: { message: "Route cannot be saved" } })
+        console.log(err)
+    })
+
+    // console.log(req.body);
+})
+
+router.get('/routesList', jwtverifier, (req, res) => {
+    const id = req.params.decodedID;
+
+    RoutesData.find({ addedBy: id }, (err, result) => {
+        if(err){
+            console.log(err);
+            res.send({ status: false, result: { message: "Error generating route list" } })
         }
         else{
             res.send({ status: true, result: result })
---
+++ schema/configs/routes.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const routesdata = mongoose.Schema({
+    routeID: {type: mongoose.Schema.Types.Mixed, required: true},
+    routeName: {type: mongoose.Schema.Types.Mixed, required: true},
+    stationList: {type: Array, required: true},
+    routePath: {type: Array, required: true},
+    dateAdded: {type: mongoose.Schema.Types.Mixed, required: true},
+    addedBy: {type: mongoose.Schema.Types.Mixed, required: true},
+    status: {type: Boolean, required: true}
+})
+
+module.exports = mongoose.model("RoutesData", routesdata, "routes");
