740464b1d487b5a66428cf442c78b73fa8e3c0e7
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-01-25 17:13:06 -0900

    Routes for Commuter Edit Profile has been Added

--- routes/commuters/index.js
+++ routes/commuters/index.js
@@ -87,4 +87,85 @@ router.get('/userinfo', jwtverifiercommuter, (req, res) => {
     })
 })
 
+router.post('/editcommutername', jwtverifiercommuter, (req, res) => {
+    const userID = req.params.userID;
+    const name = req.body.name
+
+    CommuterData.updateOne({ userID: userID }, { name: name }, (err, result) => {
+        if(err){
+            console.log(err)
+            req.send({ status: false, message: "Cannot update details" })
+        }
+        else{
+            res.send({ status: true, message: "Name has been updated" })
+        }
+    })
+})
+
+router.post('/editcommuteremail', jwtverifiercommuter, (req, res) => {
+    const userID = req.params.userID;
+    const email = req.body.email
+    const password = req.body.password
+
+    CommuterData.updateOne({ userID: userID, password: password }, { email: email }, (err, result) => {
+        if(err){
+            console.log(err)
+            req.send({ status: false, message: "Cannot update details" })
+        }
+        else{
+            // console.log(result)
+            if(result.modifiedCount > 0){
+                res.send({ status: true, message: "Email has been updated" })
+            }
+            else{
+                res.send({ status: false, message: "Password is incorrect" })
+            }
+        }
+    })
+})
+
+router.post('/editcommutercontactnumber', jwtverifiercommuter, (req, res) => {
+    const userID = req.params.userID;
+    const contactnumber = req.body.contactnumber
+    const password = req.body.password
+
+    CommuterData.updateOne({ userID: userID, password: password }, { contactnumber: contactnumber }, (err, result) => {
+        if(err){
+            console.log(err)
+            req.send({ status: false, message: "Cannot update details" })
+        }
+        else{
+            // console.log(result)
+            if(result.modifiedCount > 0){
+                res.send({ status: true, message: "Contact Number has been updated" })
+            }
+            else{
+                res.send({ status: false, message: "Password is incorrect" })
+            }
+        }
+    })
+})
+
+router.post('/editcommuterpassword', jwtverifiercommuter, (req, res) => {
+    const userID = req.params.userID;
+    const newpassword = req.body.newpassword
+    const password = req.body.password
+
+    CommuterData.updateOne({ userID: userID, password: password }, { password: newpassword }, (err, result) => {
+        if(err){
+            console.log(err)
+            req.send({ status: false, message: "Cannot update details" })
+        }
+        else{
+            // console.log(result)
+            if(result.modifiedCount > 0){
+                res.send({ status: true, message: "Password has been updated" })
+            }
+            else{
+                res.send({ status: false, message: "Current Password is incorrect" })
+            }
+        }
+    })
+})
+
 module.exports = router;
